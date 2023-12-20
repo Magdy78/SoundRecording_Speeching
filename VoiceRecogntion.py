1b9292013efa907e8e983b9aed9657c29bd9c01c
Ahmed Magdy <azzam9778@gmail.com>
2023-12-20 15:06:29 +0200

    Speech Recognition Using Sphinx

--- VoiceRecogntion.py
+++ VoiceRecogntion.py
@@ -1,21 +1,20 @@
 import speech_recognition as sr
 
+def transcribe_audio(audio_file_path):
+    recognizer = sr.Recognizer()
+
+    with sr.AudioFile(audio_file_path) as source:
+        audio_data = recognizer.record(source)
+
+    try:
+        # Use Sphinx for speech recognition
+        text = recognizer.recognize_sphinx(audio_data)
+        return text
+    except sr.UnknownValueError:
+        print("Sphinx could not understand the audio")
+    except sr.RequestError as e:
+        print(f"Could not request results from Sphinx; {e}")
+
 audio_file_path = "output_filtered.wav"
-recognizer = sr.Recognizer()
-recognizer.energy_threshold = 300
-
-audio_file = sr.AudioFile(audio_file_path)
-print(type(audio_file))
-
-with audio_file as source:
-    recognizer.adjust_for_ambient_noise(source, duration=0.5)
-    audio_file_data = recognizer.record(source)
-
-try:
-    # Recognize speech using Sphinx
-    text = recognizer.recognize_sphinx(audio_file_data)
-    print("Sphinx recognition result:", text)
-except sr.UnknownValueError:
-    print("Sphinx could not understand the audio")
-except sr.RequestError as e:
-    print(f"Sphinx request failed; {e}")
+transcribed_text = transcribe_audio(audio_file_path)
+print(transcribed_text)
